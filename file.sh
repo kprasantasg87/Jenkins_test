3d8e201e20d642b9380e760177888d1a9aef74ee
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2023-08-07 06:53:54 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -5,7 +5,7 @@ filevalue=`ls -l a.txt|tr -s ' '|cut -d ' ' -f7`
 if [ $filevalue = $datevalue ]
 then 
 echo "file is available"
-echo "Thank you linux OS"
+echo "Thank you linux OSss"
 else
 echo "file is not available"
 fi 
