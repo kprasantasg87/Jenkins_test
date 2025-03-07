40735db5c5e144d5fb8f0ec6eb455284a64ab7ca
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-07 17:29:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider aws {
     region="ap-south-1"
-    access_key="AKIAXBL5DJOO7PXUDTG3"
-    secret_key="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"
+    access_key="AKIA2YIB77KPOX23WKHU"
+    secret_key="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"
 }
 
 resource aws_vpc "MVPC"{
