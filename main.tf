68ec061a3ef6ccdf398befbb31c5de26f5f775ab
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-10 17:43:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.90.0"  # Change this to 5.x if needed
-    }
-  }
-}
+
 
 provider "aws" {
   region = "ap-southeast-1"
