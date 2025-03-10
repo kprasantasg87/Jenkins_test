c6ab1927412625499aa710b41df2f9d823b6b185
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-10 15:01:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ provider aws {
 resource "aws_instance" "awsinstance1" {
   ami                         = "ami-099b3d23e336c2e83"
   instance_type               = "t2.micro"
-  key_name                    = "jenkins"
    associate_public_ip_address = true
   availability_zone           = "ap-south-1a"
   
