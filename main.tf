f2500fbc12a5f3053066c238d417e3309230aef2
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-10 15:42:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.90.0"  # Change this to 5.x if needed
+    }
+  }
+}
 
-terraform{
-required_providers{
-aws={
-source = "hashicorp/aws"
-version = "~>4.0"
-}
-}
-}
-provider aws {
-    region="ap-south-1"
-  
+provider "aws" {
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "awsinstance1" {
   ami                         = "ami-099b3d23e336c2e83"
   instance_type               = "t2.micro"
-   associate_public_ip_address = true
+  associate_public_ip_address = true
   availability_zone           = "ap-south-1a"
-  
-  
 }
-
 
