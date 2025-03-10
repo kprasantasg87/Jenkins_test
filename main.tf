ff028931ad913d1b70ad1c3677ce7ce34ad9a5bf
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-10 15:33:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
+
+terraform{
+required_providers{
+aws={
+source = "hashicorp/aws"
+version = "~>4.0"
+}
+}
+}
 provider aws {
     region="ap-south-1"
   
 }
-
-
-
-
 
 resource "aws_instance" "awsinstance1" {
   ami                         = "ami-099b3d23e336c2e83"
