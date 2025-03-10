e233bc6a38a4cc280d4b83f752a97e4996915a4f
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-10 17:32:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "aws_instance" "awsinstance1" {
   ami                         = "ami-099b3d23e336c2e83"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  availability_zone           = "ap-south-1a"
+
 }
 
