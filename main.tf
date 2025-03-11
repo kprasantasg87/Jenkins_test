438c2beef31029ab649903c01fe4607402bc5b4d
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2025-03-11 16:55:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,61 @@
 provider "aws" {
 region= "ap-southeast-1"
-access_key= "AKIA2YIB77KPI4VEA2EB"
-secret_key= "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"
+
+}resource "aws_vpc" "VPC"{
+cidr_block=  "10.0.0.0/24"
 }
-resource "aws_instance" "instance" {
-    ami = "ami-0b5a4445ada4a59b1"
-    instance_type = "t2.micro"
-    associate_public_ip_address = true
+
+resource "aws_subnet" "SUB" {
+   vpc_id = aws_vpc.VPC.id
+   availability_zone = "ap-southeast-1a"
+   cidr_block = "10.0.0.0/28"
+}
+ 
+
+
+resource "aws_internet_gateway" "IGW"{
+   vpc_id = aws_vpc.VPC.id
+}
+resource "aws_route_table" "RT"{
+   vpc_id = aws_vpc.VPC.id
+   route{
+      gateway_id =  aws_internet_gateway.IGW.id
+      cidr_block = "0.0.0.0/0"
+   }
+   tags = {
+     Name ="MYRT"
+   }
+}
+
+resource "aws_route_table_association" "ARTA" {
+   subnet_id = aws_subnet.SUB1.id
+   route_table_id = aws_route_table.RT.id
+}
+
+
+
+resource "aws_ebs_volume" "EBS"{
+   type = "gp3"
+   size = 15
+   availability_zone = "ap-southeast-1b"
+   tags = {
+      Name ="EBS"
+   }
+}
+
+resource "aws_instance" "Instance"{
+
+ami= "ami-0c50b6f7dc3701ddd"
+instance_type = "t2.micro"
+key_name = "EBS-VOL"
+availability_zone = "ap-southeast-1a"
+subnet_id = aws_subnet.SUB1.id
+associate_public_ip_address =  true
+   tags = {
+      Name = "Instance"
+   }
+user_data = <<EOF
+#!/bin/bash
+sudo yum install nginx -y
+EOF
 }
